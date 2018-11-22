9dbbf60e5bf063734bb03e2f14b8f0470505cfad
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-22 13:56:11 -0200

    Update passfisher.sh

--- passfisher.sh
+++ passfisher.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo ""
 test /usr/bin/python  && echo "[+] Python ok" || apt-get -y install python
 test /usr/bin/perl && echo "[+] Perl ok" || apt-get -y install perl
-echo ""
+
 
 if [$1 = ""]; then
     printf("if not")
