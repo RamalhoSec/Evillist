d0c2bea730fad1d6fa6092a873898cbdc670c729
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-22 14:41:03 -0200

    Update and rename passfisher.sh to evillist.sh

---
+++ evillist.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# encoding utf-8
+# Created by: Acr4n1us
+# Contact: https://github.com/RamalhoSec
+# Bitcoin: 
+
+#color
+red='\033[31;1m'
+green='\033[32;1m'
+cyan='\033[36;1m'
+end='\033[m'
+
+#logo
+F_LOGO(){
+  cat <<LOGO | less
+  ```
+          ^         
+         | |        
+       @#####@      
+     (###   ###)-.  
+   .(###     ###) \ 
+  /  (###   ###)   )
+ (=-  .@#####@|_--" 	This is a hold up! Put all your money in the
+ /\    \_|l|_/ (\   	disk drive slot, and no one gets hurt!
+(=-\     |l|    /   
+ \  \.___|l|___/        
+ /\      |_|   /    
+(=-\._________/\    
+ \             /    
+   \._________/     
+     #  ----  #     
+     #   __   #       
+     \########/      
+  ´´´
+LOGO
+}
