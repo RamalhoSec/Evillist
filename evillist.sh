b1d58609b5363781f246d670deebc26d993d58f8
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-22 14:48:01 -0200

    Update evillist.sh

--- evillist.sh
+++ evillist.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 # encoding utf-8
 # Created by: Acr4n1us
-# Contact: https://github.com/RamalhoSec
-# Bitcoin: 
+# Contact & Changelog: https://github.com/RamalhoSec
+# Bitcoin:
 
-#color
+
+
+#::required root
+[ "$UID" = "1" ] && { echo "Please run the script at root"; exit 1 ;}
+
+#::color
 red='\033[31;1m'
 green='\033[32;1m'
 cyan='\033[36;1m'
 end='\033[m'
 
-#logo
+#::logo
 F_LOGO(){
   cat <<LOGO | less
-  ```
-          ^         
-         | |        
-       @#####@      
-     (###   ###)-.  
-   .(###     ###) \ 
+          ^
+         | |
+       @#####@
+     (###   ###)-.
+   .(###     ###) \
   /  (###   ###)   )
  (=-  .@#####@|_--" 	This is a hold up! Put all your money in the
  /\    \_|l|_/ (\   	disk drive slot, and no one gets hurt!
-(=-\     |l|    /   
- \  \.___|l|___/        
- /\      |_|   /    
-(=-\._________/\    
- \             /    
-   \._________/     
-     #  ----  #     
-     #   __   #       
-     \########/      
-  ´´´
+(=-\     |l|    /
+ \  \.___|l|___/
+ /\      |_|   /
+(=-\._________/\
+ \             /
+   \._________/
+     #  ----  #
+     #   __   #
+     \########/
 LOGO
 }
