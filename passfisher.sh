31f5cf8d1d4df5d5966bbc4d11a87456006dc524
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-22 13:48:11 -0200

    Update passfisher.sh

--- passfisher.sh
+++ passfisher.sh
@@ -6,13 +6,7 @@ test /usr/bin/perl && echo "[+] Perl ok" || apt-get -y install perl
 echo ""
 
 if [$1 = ""]; then
-    echo "[!] Uso: ./passfisher.sh {MIN} {MAX} {LENGHT}"
-    echo "[!] Examplo: ./passfisher.sh 10 10 500"
-    echo "[!] Info: ./passfisher.sh {Tamanho minimo} {Tamanho Maximo} {Numero de Linhas}"
-    echo ""    
+    printf("if not")
 else
-    python main.py
-    victiminfo=$(<victim.lst)
-    perl main.pl -ml $1 -l $2 -mm $3 -m $victiminfo --random
-    ls -lsha wordlist.txt
+    printf("if yes")
 fi
